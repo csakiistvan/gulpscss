2d1790b659fab5f961e3d82dad3a8509e863ba3a
Istvan Csaki <steven.csaki@gmail.com>
2023-07-05 11:18:26 +0200

    Update commands

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,58 @@ const
   gulp = require('gulp'),
   sourcemaps = require('gulp-sourcemaps'),
   autoprefixer = require('gulp-autoprefixer'),
+  uglify = require('gulp-uglify'),
   sass = require('gulp-sass')(require('sass'));
 
 const paths = {
-  theme: {
-    scss: 'scss/app.scss',
-    dest: 'css',
-    watch: 'scss/**/*.scss',
+  styles: {
+    scss: 'src/scss/app.scss',
+    dest: 'dist/css',
+    watch: 'src/scss/**/*.scss'
+  },
+  images: {
+    src: "src/images/**/*.*",
+    dest: "dist/images/"
+  },
+  js: {
+    src: "src/js/**/*.*",
+    dest: "dist/js/"
   }
 }
+
 // Styles
 gulp.task('appStyle', (done) => {
   gulp
-    .src(paths.theme.scss)
+    .src(paths.styles.scss)
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(autoprefixer('last 2 version'))
+    .pipe(autoprefixer('last 3 version'))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.theme.dest));
+    .pipe(gulp.dest(paths.styles.dest));
   done();
 })
 
+// Images
+gulp.task('images', (done) => {
+  gulp
+    .src(paths.images.src)
+    .pipe(gulp.dest(paths.images.dest));
+  done();
+})
+
+// Javascript
+gulp.task('js', (done) => {
+  gulp
+    .src(paths.js.src)
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.js.dest));
+  done();
+})
+
+gulp.task('build', gulp.series('js', 'images', 'appStyle'));
+
 gulp.task('watch', function () {
-  gulp.watch(paths.theme.watch, gulp.task('appStyle'))
+  gulp.watch(paths.styles.watch, gulp.task('appStyle'))
+  gulp.watch(paths.images.src, gulp.task('images'))
+  gulp.watch(paths.js.src, gulp.task('js'))
 });
